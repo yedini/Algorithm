907fcd9c551b42c86dc6d2316b794449d98ed737
yedini <dcng@naver.com>
2022-01-19 22:24:17 +0900

    chap12.구현

--- 기출문제/Chap12/chap12-7.py
+++ 기출문제/Chap12/chap12-7.py
@@ -2,6 +2,6 @@ nums = list(input())
 nums = list(map(int, nums))
 length = int(len(nums)/2)
 
-if sum(nums[:length]) == sum(nums[length:]):
+if sum(nums[:length]) == sum(nums[length:]):     # 반 나눠서 길이 같으면 lucky 출력
     print("LUCKY")
 else: print("READY")
--- 기출문제/Chap12/chap12-8.py
+++ 기출문제/Chap12/chap12-8.py
@@ -11,6 +11,6 @@ result = ''
 for i in range(len(letters)):
     result = result + letters[i]   # 알파벳 오름차순으로 붙이기
 
-nums = list(map(int, nums))
+nums = list(map(int, nums))        # 정수화
 
 print(result+str(sum(nums)))       # 정렬된 알파벳에 숫자합 합쳐서 출력
