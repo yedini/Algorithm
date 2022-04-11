027b6719186f6990b62ac7eea6274b21e2bf1dc2
yedini <dcng@naver.com>
2022-04-12 01:39:07 +0900

    0412

---
+++ 예제2회차/chap5/영역구하기.py
@@ -0,0 +1,29 @@
+m, n, k = map(int, input().split())
+graph = [[0]*n for _ in range(m)]
+
+for _ in range(k):
+    xs, ys, xe, ye = map(int, input().split())
+    for i in range(ys, ye):
+        for j in range(xs, xe):
+            graph[i][j] = 1    # 1: 칠해진 구간
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+def dfs(y, x, a):
+    graph[y][x]=1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx>=0 and nx<n and ny>=0 and ny<m and graph[ny][nx] == 0:
+            a = dfs(ny, nx, a+1)
+    return a
+
+result = []
+for i in range(m):
+    for j in range(n):
+        if graph[i][j] == 0:
+            result.append(dfs(i, j, 1))
+
+
+print(len(result))
+print(*sorted(result))
