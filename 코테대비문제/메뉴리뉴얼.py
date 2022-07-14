da43f80e2947b93a98e73d212c9383f114ce46c0
yedini <dcng@naver.com>
2022-07-14 21:08:10 +0900

    0714

--- 코테대비문제/메뉴리뉴얼.py
+++ 코테대비문제/메뉴리뉴얼.py
@@ -1,18 +1,19 @@
 from itertools import combinations
 def solution(orders, course):
-    cand = dict()
     answer = []
-    for n in course:
+    for n in course:   
+        cand = dict()     # 후보 메뉴구성 + 시킨 횟수를 cand에 담음
         for order in orders:
             if len(order) >= n:
                 l = list(combinations(list(order), n))
                 for ll in l:
-                    print(type(ll))
-                    if ll in cand.keys():
-                        cand[ll] += 1
+                    lll = ''.join(sorted(ll))
+                    if lll in cand.keys():
+                        cand[lll] += 1
                     else:
-                        cand[ll] = 1
-    for key, value in cand.items():
-        if value >= 2:
-            answer.append(key)
-    return ll
+                        cand[lll] = 1
+        if cand:
+            maxv = max(cand.values())  # cand에 담긴 후보 메뉴구성 중 가장 많이 나간 구성
+            if maxv >= 2:  # 2번 이상 시킨 경우에만 해당!
+                answer = answer + [k for k in cand.keys() if cand[k] == maxv] # answer에 메뉴구성을 추가
+    return sorted(answer)
