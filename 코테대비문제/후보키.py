a9d439f18c66959030af1fd1c2ba24d8877ca47d
yedini <dcng@naver.com>
2022-10-13 16:38:03 +0900

    1013

--- 코테대비문제/후보키.py
+++ 코테대비문제/후보키.py
@@ -1,19 +1,22 @@
 from itertools import combinations
-
 def solution(relation):
-    answer = 0
-    l = len(relation)
-    relation = list(zip(*relation))  # 행과 열 바꾸기
-    for i in range(1, l+1):
-        if len(relation) >= i:
-            cand = list(combinations(range(len(relation)), i))
-            while cand:
-                c = cand.pop(0)
-                temp = [relation[j] for j in range(len(relation)) if j in c]
-                if len(temp) > 0 and len(list(zip(*temp))) == len(set(zip(*temp))):
-                    print(temp)
-                    answer += 1
-                    relation = [relation[j] for j in range(len(relation)) if j not in c]
-                    cand = [cc for cc in cand if len(set(c).intersection(set(cc)))==0]
-                    print(cand)
-    return answer
+    new = list(zip(*relation))   # 행과 열 바꾸기
+    cand = []
+    for i in range(1, len(new)+1):
+        cand = cand + list(combinations(range(len(new)), i))
+    keys = []
+    while cand:
+        c = cand.pop(0)
+        temp = [new[j] for j in range(len(new)) if j in c]
+
+        # 유일성 확인
+        if len(temp)>0 and len(list(zip(*temp))) == len(set(zip(*temp))):
+            # 최소성 확인
+            check = True
+            for k in keys:
+                if set(k).issubset(set(c)):   # k가 c의 부분집합인지 확인
+                    check = False
+                    break
+            if check:
+                keys.append(c)
+    return len(keys)
